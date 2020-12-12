8fc808c08d0ff0e97cb132aabb7b9a22ae4099ef
Denys Sennikov <denys.sennikov@creatoriq.com>
2020-12-12 10:11:35 +0200

    Operator class

---
+++ src/com/academy/telesens/Lesson5_OOP/OOP/Operator.java
@@ -0,0 +1,32 @@
+package com.academy.telesens.Lesson5_OOP.OOP;
+
+public class Operator {
+
+    String name;
+    String address;
+
+    public Operator(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Operator (){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+
+}
