2298100ddba93b534ba6aaf1de86b02927e75c39
Denys Sennikov <denys.sennikov@creatoriq.com>
2021-01-16 08:43:32 +0200

    Finish some tasks from Hometask

--- src/com/academy/telesens/Lesson10_GenericAndColections/Generic/HomeTask/ListFirst.java
+++ src/com/academy/telesens/Lesson10_GenericAndColections/Generic/HomeTask/ListFirst.java
@@ -1,6 +1,8 @@
 package com.academy.telesens.Lesson10_GenericAndColections.Generic.HomeTask;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class ListFirst {
@@ -13,7 +15,9 @@ public class ListFirst {
         }
         System.out.println(list);
 
-        list.add("Philip, Joseph, Leon");
+        list.add("Philip");
+        list.add("Joseph");
+        list.add("Leon");
         System.out.println(list);
         list.remove("Leonid");
         list.remove("Veronika");
@@ -21,11 +25,30 @@ public class ListFirst {
 
         for (int j = 0; j < list.size(); j++) {
             if (list.get(j) == "Helen") {
-               list.remove(list.get(j));
-               list.add(j, "Elizabet");
-               break;
+                list.remove(list.get(j));
+                list.add(j, "Elizabet");
+                break;
+            }
+        }
+        System.out.println(list);
+        list.sort(String::compareTo);
+        System.out.println(list);
+        Collections.reverse(list);
+        System.out.println(list);
+
+        for (int k = 0; k < list.size(); k++) {
+            if (list.get(k) == "Andry") {
+                System.out.println("Элемент Andry встречается в списке");
+            }
+        }
+        System.out.println(list.contains("Andry"));
+
+        for (int l = 0; l < list.size(); l++) {
+            if (list.get(l) != "Helen") {
+                list.remove(l);
             }
         }
         System.out.println(list);
     }
 }
+
